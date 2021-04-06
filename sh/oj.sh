26ed2d3cd7e61e494caf583a2192f4e13f3bb960
kaminotsukai <japanbritish.1107@gmail.com>
2021-04-06 21:15:50 +0900

    first commit

---
+++ sh/oj.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# testフォルダがある場合は削除する(ojコマンドがtestフォルダがあるとエラーになるから)
+rm -rf test
+
+# testcaseをdownload
+oj d $1
+
+# ファイルを作成する
+array=( `echo $1 | tr -s '/' ' '`)
+last_index=`expr ${#array[@]} - 1`
+
+# テンプレートのコピー
+if [ -e "${array[${last_index}]}.php" ]; then
+  echo "ファイルが存在するためテンプレートのコピーをスキップしました"
+else
+  cp $PWD/sh/template.php "${array[${last_index}]}.php"
+fi
